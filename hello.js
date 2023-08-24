24d7bba2897a89f38d480561ca667c56d5b4a55c
Andrew Turn <arturn63021@yahoo.com>
2023-08-24 17:42:47 -0500

    changed to quote

--- hello.js
+++ hello.js
@@ -4,7 +4,9 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+console.log('Quoth the Raven "Nevermore"');
+/*this is my code
+/*
 
 /*  
 Next, PLAY! 
